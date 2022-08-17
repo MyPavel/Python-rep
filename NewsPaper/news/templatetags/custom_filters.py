ac881c17ddf7d9475eaa2b7abb6eb9f4da15d079
Pavel M <oli.twist@yahoo.com>
2022-08-17 15:35:59 +0300

    D3 Homework

---
+++ NewsPaper/news/templatetags/custom_filters.py
@@ -0,0 +1,17 @@
+from django import template
+
+
+register = template.Library()
+
+F_WORDS = ['вакуум', 'вакуума', 'Цыгане', 'цыганского']
+
+
+@register.filter()
+def censor(in_text):
+   for word in in_text.split():
+      if word in F_WORDS:
+         in_text = in_text.replace(
+            word,
+            f'{word[0]}{"*" * (len(word) - 1)}'
+         )
+   return in_text
