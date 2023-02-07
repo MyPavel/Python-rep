f2b29208e54c2e074c20d30167fac00b65835b2c
Pavel M <107254988+MyPavel@users.noreply.github.com>
2023-02-07 10:58:50 +0300

    Delete NewsPaper directory

--- NewsPaper/news/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Post, Author, Category, PostCategory, CategorySubscribers
-
-
-admin.site.register(Post)
-admin.site.register(Author)
-admin.site.register(Category)
-admin.site.register(PostCategory)
-admin.site.register(CategorySubscribers)
