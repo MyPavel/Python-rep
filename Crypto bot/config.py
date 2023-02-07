1a7a662e20d174398e87bad622b1f2ef89cfffdb
Pavel M <107254988+MyPavel@users.noreply.github.com>
2023-02-07 10:58:29 +0300

    Delete Crypto bot directory

--- Crypto bot/config.py
+++
@@ -1,8 +0,0 @@
-# This Python file uses the following encoding: utf-8+
-
-keys = {
-    'доллар': 'USD',
-    'евро': 'EUR',
-    'рубль': 'RUB'
-}
-TOKEN = "5480811776:AAHlB7fazpAsVTaO2VY5WgSGx-7xefEu6oI"
