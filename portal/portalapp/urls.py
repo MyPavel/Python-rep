4239f7418843ccc0e3681b3c235613c3a00b7f6d
Pavel M <107254988+MyPavel@users.noreply.github.com>
2023-02-07 10:49:53 +0300

    Delete portal directory

--- portal/portalapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import (ArticleList, ArticleDetail)
-
-urlpatterns = [
-   path('', ArticleList.as_view(), name='article_list'),
-   path('<int:pk>', ArticleDetail.as_view(), name='article_detail'),
-    ]
